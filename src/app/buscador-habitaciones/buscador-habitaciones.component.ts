51430227694538642a7373f5f8ca961231c46bf0
fergil <guerragil.fernanda@gmail.com>
2019-05-05 17:08:51 +0200

    Html del buscador - mostrar habitaciones que vienen con el objeto response Componente del buscador - recuperación de habitaciones del backend y nuevos imports Http y FormsModule App Module - Inclusión del HttpClientModule

--- src/app/buscador-habitaciones/buscador-habitaciones.component.ts
+++ src/app/buscador-habitaciones/buscador-habitaciones.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-buscador-habitaciones',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buscador-habitaciones.component.css']
 })
 export class BuscadorHabitacionesComponent implements OnInit {
+  get search(): () => void {
+    return this._search;
+  }
 
-  constructor() { }
+  set search(value: () => void) {
+    this._search = value;
+  }
+
+
+  response: any;
+  constructor(private http: HttpClient) { }
+
 
   ngOnInit() {
+
   }
+
+  private _search = () => {
+    this.http.get('http://localhost:8080/api/v0/hotels/search')
+      .subscribe((response) => {
+        this.response = response;
+      }); }
 
 }
