6d6f02958269602c69328872ade64c295e3c6df0
fatandaz <fatandaz@gmail.com>
2019-05-07 02:05:57 +0200

    Visualizar todas habitaciones - issue #10

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import {MatDialog, MatDialogConfig} from '@angular/material';
 import {HabitacionService} from './habitacion.service';
 import {VerHabitacionDialogComponent} from '../ver-habitacion-dialog/ver-habitacion-dialog.component';
 import {Habitacion} from '../ver-habitacion-dialog/habitacion.model';
+import {RoomModel} from './room.model';
+import {RoomsModel} from './rooms.model';
 
 @Component({
   templateUrl: 'home.component.html',
@@ -13,13 +15,15 @@ export class HomeComponent {
   static URL = 'home';
 
   habitacion: Habitacion;
-
-  title = 'Articles Management';
-  columns = ['code', 'description', 'retailPrice', 'stock'];
-  data: Habitacion[];
+  data: RoomsModel[];
+  title = 'Rooms ';
+  columns = ['id', 'tipo', 'precioHora', 'precioDia' ];
   dialogConfig: MatDialogConfig;
 
   constructor(private dialog: MatDialog, private habitacionService: HabitacionService) {
+    this.habitacionService.readAll().subscribe(
+      data => this.data = data
+    );
   }
 
 
@@ -32,5 +36,12 @@ export class HomeComponent {
     };
 
     this.dialog.open(VerHabitacionDialogComponent, this.dialogConfig);
+  }
+
+  readAll() {
+    this.habitacionService.readAll().subscribe(
+      data => this.data = data
+    );
+    console.log('data: ' +  this.data);
   }
 }
